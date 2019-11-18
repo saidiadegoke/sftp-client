1e8f8c12207199db982a033035b4de1049c65480
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-11-18 13:01:49 +0100

    Setup project with Spring boot

---
+++ src/main/java/ng/com/helloworld/SftpClientApplication.java
@@ -0,0 +1,13 @@
+package ng.com.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpClientApplication.class, args);
+	}
+
+}
