11c94e489b27dd99db534cef997ea23c56c23068
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-11-26 15:40:47 +0100

    Setup test app for secret key generation

--- src/main/java/com/flexipgroup/app/Application.java
+++ src/main/java/com/flexipgroup/app/Application.java
@@ -1,6 +1,10 @@
 package com.flexipgroup.app;
 
+import java.io.IOException;
+
+import com.flexipgroup.app.cipher.SecretKeyGen;
 import com.flexipgroup.app.config.ConfigurationFile;
+import com.flexipgroup.app.service.MonitorDirectory;
 
 public class Application {
 
@@ -8,7 +12,7 @@ public class Application {
 		
 		ConfigurationFile config = new ConfigurationFile();
 		
-		while(true) {
+		if(true) {
 			/**
 			 * 
 			 * TODO
@@ -32,12 +36,23 @@ public class Application {
 			 */
 			System.out.println("Watch file changes on FileObserver");
 			try {
+				//String secretKey = new SecretKeyGen().run();
 				Thread.sleep(config.MAIN_POLL_INTERVAL);
 			} catch (InterruptedException e) {
 				// TODO 
 				// Log everything happening, especially the errors.
 				e.printStackTrace();
 			}
+		}
+		
+		try {
+			MonitorDirectory.run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
---
+++ src/main/java/com/flexipgroup/app/cipher/SecretKeyGen.java
@@ -0,0 +1,50 @@
+package com.flexipgroup.app.cipher;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class SecretKeyGen {
+	
+	private static SecretKey generateSecretKey1 ()
+	{
+		final SecureRandom prng = new SecureRandom();
+	    final byte[] aes128KeyData  = new byte[128 / Byte.SIZE];
+	    prng.nextBytes(aes128KeyData);    
+	    final SecretKey aesKey = new SecretKeySpec(aes128KeyData, "AES");
+	    return aesKey;
+	}
+
+ public static SecretKey generateSecretKey() {
+  KeyGenerator keyGen = null;
+  try {
+   /*
+    * Get KeyGenerator object that generates secret keys for the
+    * specified algorithm.
+    */
+   keyGen = KeyGenerator.getInstance("AES");
+  } catch (NoSuchAlgorithmException e) {
+   e.printStackTrace();
+  }
+
+  /* Initializes this key generator for key size to 256. */
+  keyGen.init(256);
+
+  /* Generates a secret key */
+  SecretKey secretKey = keyGen.generateKey();
+
+  return secretKey;
+ }
+
+ public String run() {
+  SecretKey secretKey = generateSecretKey();
+  String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+  System.out.println(encodedKey);
+  return encodedKey;
+  
+ }
+}
