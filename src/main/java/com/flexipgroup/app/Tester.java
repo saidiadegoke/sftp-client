c5a70da33f0ab3e71eff401d5cea7aa4b82e8318
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-11-29 12:48:28 +0100

    Modify put folder on sftp server

--- src/main/java/com/flexipgroup/app/Tester.java
+++ src/main/java/com/flexipgroup/app/Tester.java
@@ -3,13 +3,19 @@ package com.flexipgroup.app;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.flexipgroup.app.config.ConfigurationFile;
 import com.flexipgroup.app.log.FileLogger;
+import com.flexipgroup.sender_client.SenderClient;
 
 
 public class Tester {
 	public static void main1(String[] args) throws IOException, TimeoutException {
+		ConfigurationFile config = new ConfigurationFile();
+
 		FileLogger logger = new FileLogger();
 		//logger.log();
-	}	
-	
+		
+		//new SenderClient().run();
+
+	}
 }
